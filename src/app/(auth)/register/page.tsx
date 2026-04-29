a501dce3ee4d826fba1b7107748852a9cebfa4de
sturza-c <christian.sturza@unine.ch>
2026-04-29 10:42:10 +0200

    Update import statements and add dynamic export

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,10 +1,12 @@
-import { Suspense } from 'react'
-import { RegisterForm } from './register-form'
+import { Suspense } from "react";
+import { RegisterForm } from "./register-form";
+
+export const dynamic = "force-dynamic";
 
 export default function RegisterPage() {
   return (
-    <Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
       <RegisterForm />
     </Suspense>
-  )
+  );
 }
